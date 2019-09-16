cb2f8e9abbf007a3232968c66c31645d34ef25dd
mirajul.islam <mirajul.islam@NAZ-TECH-PC-101.ntdac.naztech.local>
2019-09-16 17:53:25 +0600

    file upload

--- Spring_Boot_File/src/main/java/com/example/demo/SpringBootMicroservicesApplication.java
+++ Spring_Boot_File/src/main/java/com/example/demo/SpringBootMicroservicesApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.example.demo.entites.FileStorageProperties;
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class SpringBootMicroservicesApplication {
 
 	public static void main(String[] args) {
