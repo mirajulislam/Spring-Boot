3d525ede41089f8bef8e53ca863868a512484c3d
miraj <miraj@DESKTOP-VP27H1P>
2019-12-06 12:07:10 +0600

    relational dropdown menu

--- CASCADING_DROPDOWN_LIST/src/main/java/com/example/demo/controller/DemoController.java
+++ CASCADING_DROPDOWN_LIST/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,17 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.services.CityService;
 import com.example.demo.services.CountryService;
 import com.example.demo.services.StateService;
 import com.google.gson.Gson;
-
+@Controller
+@RequestMapping(value = { "", "demo" })
 public class DemoController {
 	
 
@@ -24,6 +28,15 @@ public class DemoController {
 	@Autowired
 	private CityService cityService;
 
+//	@RequestMapping(method = RequestMethod.GET)
+//	public ModelAndView country() {
+////		modelMap.put("countries", countryService.findAll());
+////		return "demo/index";
+//		ModelAndView model = new ModelAndView();
+//		model.setViewName("demo/index");
+//		return model;
+//	}
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(ModelMap modelMap) {
 		modelMap.put("countries", countryService.findAll());
